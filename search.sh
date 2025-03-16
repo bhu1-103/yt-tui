7b51cf260e150b57f4a4be3aa4c1b8dad5b3f72f
bhu1-103 <103.bhuvan@gmail.com>
2025-03-16 23:09:25 +0530

    downloads audio

--- search.sh
+++ search.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/zsh
 
-echo -n "enter search query: "
-read query
-wget https://inv.nadeko.net/search?q=$query
+echo -n "Enter search query: "
+read search_query 
+formatted_query=$(echo "$search_query" | sed 's/ /+/g')
+USER_AGENT="Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0"
+echo "Searching for: $search_query ..."
+wget --user-agent="$USER_AGENT" -qO- "https://inv.nadeko.net/search?q=$formatted_query" -qO index.html
+
+tag=$(cat index.html | grep 'href="/watch?' | head -n 1 | awk -F "=" '{print $4}' | awk -F '"' '{print $1}')
+
+yt-dlp -f "bestaudio" "youtube.com/watch?v=$tag"
