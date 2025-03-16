844c7d2ce9f5339389bf1008a454db5b89f9120f
bhu1-103 <103.bhuvan@gmail.com>
2025-03-16 23:15:21 +0530

    downloads audio in m4a format

--- search.sh
+++ search.sh
@@ -5,8 +5,9 @@ read search_query
 formatted_query=$(echo "$search_query" | sed 's/ /+/g')
 USER_AGENT="Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0"
 echo "Searching for: $search_query ..."
+
 wget --user-agent="$USER_AGENT" -qO- "https://inv.nadeko.net/search?q=$formatted_query" -qO index.html
 
-tag=$(cat index.html | grep 'href="/watch?' | head -n 1 | awk -F "=" '{print $4}' | awk -F '"' '{print $1}')
+id=$(cat index.html | grep 'href="/watch?' | head -n 1 | awk -F "=" '{print $4}' | awk -F '"' '{print $1}')
 
-yt-dlp -f "bestaudio" "youtube.com/watch?v=$tag"
+yt-dlp -f "bestaudio[ext=m4a]" --embed-metadata --embed-thumbnail --add-metadata "youtube.com/watch?v=$id"
