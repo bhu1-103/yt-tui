261c4270b5a1383f203668dc880f2eace603b5db
bhu1-103 <103.bhuvan@gmail.com>
2025-03-18 01:09:31 +0530

    fzf integration

---
+++ search2.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/zsh
+
+echo -n "Enter search query: "
+read search_query 
+formatted_query=$(echo "$search_query" | sed 's/ /+/g')
+USER_AGENT="Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0"
+echo "Searching for: $search_query ..."
+
+echo "link: https://inv.nadeko.net/search?q=$formatted_query"
+
+wget --user-agent="$USER_AGENT" -qO- "https://inv.nadeko.net/search?q=$formatted_query" -qO index.html
+
+id=$(cat index.html | grep 'a href="/watch?' | grep -viE 'sub|subs|subtitles|unofficial' | head -n 10 | awk -F "=" '{print $3}' | awk -F '"' '{print $1}' | fzf)
+
+echo $id
